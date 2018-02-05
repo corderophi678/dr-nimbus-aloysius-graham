5e0347290a784fc9523e62786fa97735d82d885b
Dr. Rich Cordero <rich@corderophilosophy.com>
2018-02-05 15:05:52 -0500

    fix typos

--- src/components/CV.js
+++ src/components/CV.js
@@ -17,7 +17,7 @@ function cvContents() {
     <React.Fragment>
       <h4 className="fw7 f4 underline">Employment</h4>
       <p>
-        Furman College, Professor of Molecular Bioinformatics, Fall 2009-Present
+        William and Hairy College, Professor of Molecular Bioinformatics, Fall 2009-Present
       </p>
       <h4 className="fw7 f4 underline">Education</h4>
       <p>
@@ -33,7 +33,7 @@ function cvContents() {
       <p>My primary research is aimed toward developing a frost-resistant strain of <em>Nepata cataria</em> (catnip), in an effort to support the well-being of cats world-wide.</p>
       <h4 className="fw7 f4 underline">Publications</h4>
       <p>"On the Merits of the cross-hatch quantum lattice", <em>BIOLOGY!</em>, v.LVI, iss. 03, pp.17-43 (2017)</p>
-      <p>"Nepat cataria in Yellow Knife: A struggle for survival and recommendations for intervention", <em>Yellow Knife Journal of Molecular Biology</em>, iss. 43, pp.322-63 (2015)</p>
+      <p>"Nepata cataria in Yellow Knife: A struggle for survival and recommendations for intervention", <em>Yellow Knife Journal of Molecular Biology</em>, iss. 43, pp.322-63 (2015)</p>
       <p>"L-tryptothymaninitrate, preserving the future for Nepata cataria", <em>Biology and Information</em>, v.xii pp.1-54 (2015)</p>
     </React.Fragment>
   )
